70b5684d5fbdfc20287330228e48d3345a66b0a2
颜晓峰 <355336363@qq.com>
2019-05-14 13:40:12 +0800

    update

--- src/miniprogram/app.js
+++ src/miniprogram/app.js
@@ -3,19 +3,15 @@ const util = require('./utils/util.js')
 const stores = require('./stores/index')
 
 App({
-  onLaunch: function () {
-    wx.cloud.init({
-      env: 'test-1b298c',
-      traceUser: true,
-    })
-  },
-
   stores: stores,
   request: request,
   util: util,
 
-  globalData: {
-    
+  onLaunch: function () {
+    wx.cloud.init({
+      env: 'test-500t',
+      traceUser: true,
+    })
   },
 
   checkLogin: function () {
@@ -26,31 +22,5 @@ App({
     } else {
       return true
     }
-  },
-
-  bannerJump: function (e) {
-    let obj = e.currentTarget.dataset.obj
-    if (obj.menuType !== undefined) {
-      switch (obj.menuType) {
-        case 'view':
-          // wx.navigateTo({
-          //   url: `/pages/webview/index?url=${encodeURIComponent(obj.menuUrl)}`
-          // })
-          break;
-        case 'click':
-          break;
-      }
-    } else if (obj.type !== undefined) {
-      switch (obj.type) {
-        case 0:
-          wx.navigateTo({
-            url: `/packageBlog/pages/detail/index?id=${obj.linkUrl.match(/blog\/(\d*)\.html/)[1]}`
-          })
-          break;
-        case 1:
-          break;
-      }
-    }
   }
-
 })
